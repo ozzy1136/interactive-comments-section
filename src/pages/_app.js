d7cd3a9d9e013fc16114bd4b87c9048dcb3f3160
Ozmar Mendoza <amfc11@gmail.com>
2023-02-15 13:18:48 -0600

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,20 @@
+import Head from "next/head";
+
 import "@styles/reset.css";
 import "@styles/global.css";
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/favicon-32x32.png"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
