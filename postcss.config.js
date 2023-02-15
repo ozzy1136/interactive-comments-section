e1d78b868f0ce4222f2649c8c7d4f934334e54a0
Ozmar Mendoza <amfc11@gmail.com>
2023-02-14 19:29:04 -0600

    Update PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,47 +1,26 @@
-export default {
-	plugins:
-		process.env.NODE_ENV === "production"
-			? {
-					"postcss-preset-env": {
-						autoprefixer: true,
-						stage: 2,
-						features: {
-							"nesting-rules": true,
-							"custom-media-queries": true,
-							"all-property": false,
-							"any-link-pseudo-class": false,
-							"custom-properties": false,
-							"font-variant-property": false,
-							"image-set-function": false,
-							"is-pseudo-class": false,
-							"logical-properties-and-values": false,
-							"not-pseudo-class": false,
-							"overflow-property": false,
-							"overflow-wrap-property": false,
-							"unset-value": false,
-						},
-					},
-			  }
-			: {
-					"postcss-preset-env": {
-						autoprefixer: true,
-						stage: 2,
-						features: {
-							"nesting-rules": true,
-							"custom-media-queries": true,
-							"all-property": false,
-							"any-link-pseudo-class": false,
-							"custom-properties": false,
-							"font-variant-property": false,
-							"gap-properties": false,
-							"image-set-function": false,
-							"is-pseudo-class": false,
-							"logical-properties-and-values": false,
-							"not-pseudo-class": false,
-							"overflow-property": false,
-							"overflow-wrap-property": false,
-							"unset-value": false,
-						},
-					},
-			  },
+const prod = process.env.NODE_ENV === "production";
+
+module.exports = {
+	plugins: {
+		"postcss-preset-env": {
+			autoprefixer: prod ? true : false,
+			stage: 2,
+			features: {
+				"nesting-rules": true,
+				"custom-media-queries": true,
+				"all-property": false,
+				"any-link-pseudo-class": false,
+				"custom-properties": false,
+				"font-variant-property": false,
+				"gap-properties": prod ? true : false,
+				"image-set-function": false,
+				"is-pseudo-class": false,
+				"logical-properties-and-values": false,
+				"not-pseudo-class": false,
+				"overflow-property": false,
+				"overflow-wrap-property": false,
+				"unset-value": false,
+			},
+		},
+	},
 };
