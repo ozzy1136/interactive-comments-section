9838949d55a6bc2b739d3816c441f3e2d00dc1b7
Ozmar Mendoza <amfc11@gmail.com>
2023-02-21 19:58:57 -0600

    Move vote selection logic to a hook file

--- src/features/CommentsSection/components/Comment.jsx
+++ src/features/CommentsSection/components/Comment.jsx
@@ -1,5 +1,3 @@
-import { useReducer } from "react";
-
 import styles from "../assets/styles/Comments.module.css";
 import MinusIcon from "../assets/icons/icon-minus.svg";
 import PlusIcon from "../assets/icons/icon-plus.svg";
@@ -8,52 +6,11 @@ import EditIcon from "../assets/icons/icon-edit.svg";
 import DeleteIcon from "../assets/icons/icon-delete.svg";
 import Avatar from "./Avatar";
 import { useCurrentUser } from "@context/CurrentUserContext";
-
-function voteInfoReducer(state, action) {
-	switch (action.type) {
-		case "upvote_clicked": {
-			return {
-				...state,
-				score:
-					state.downvotePressed === "true"
-						? state.score + 2
-						: state.upvotePressed === "false"
-						? state.score + 1
-						: state.score - 1,
-				upvotePressed:
-					state.upvotePressed === "false" ? "true" : "false",
-				downvotePressed:
-					state.downvotePressed === "true"
-						? "false"
-						: state.downvotePressed,
-			};
-		}
-		case "downvote_clicked": {
-			return {
-				...state,
-				score:
-					state.upvotePressed === "true"
-						? state.score - 2
-						: state.downvotePressed === "false"
-						? state.score - 1
-						: state.score + 1,
-				downvotePressed:
-					state.downvotePressed === "false" ? "true" : "false",
-				upvotePressed:
-					state.upvotePressed === "true"
-						? "false"
-						: state.upvotePressed,
-			};
-		}
-		default: {
-			throw new Error("Unknown action: " + action.type);
-		}
-	}
-}
+import useVoteInfo from "../hooks/useVoteInfo";
 
 export default function Comment({ data, commenter }) {
 	const currentUser = useCurrentUser();
-	const [voteInfo, dispatchVoteInfo] = useReducer(voteInfoReducer, {
+	const [voteInfo, dispatchVoteSelection] = useVoteInfo({
 		score: data.score,
 		upvotePressed: "false",
 		downvotePressed: "false",
@@ -61,96 +18,93 @@ export default function Comment({ data, commenter }) {
 
 	const isCurrentUser = currentUser.username === data.user.username;
 
-	function handleVote(e) {
-		if (e.currentTarget.ariaLabel === "Up vote") {
-			dispatchVoteInfo({ type: "upvote_clicked" });
-		}
-		if (e.currentTarget.ariaLabel === "Down vote") {
-			dispatchVoteInfo({ type: "downvote_clicked" });
-		}
-	}
-
 	return (
-		<article className={styles.comment_container}>
-			<header className={styles.comment_details_container}>
-				<Avatar
-					src={data.user.image.webp}
-					username={data.user.username}
-				/>
-				<span className={styles.comment_details_username}>
-					{data.user.username}
-				</span>
-				{isCurrentUser && (
-					<span className={styles.comment_details_label}>
-						<small>you</small>
+		<>
+			<article className={styles.comment_container}>
+				<header className={styles.comment_details_container}>
+					<Avatar
+						src={data.user.image.webp}
+						username={data.user.username}
+					/>
+					<span className={styles.comment_details_username}>
+						{data.user.username}
 					</span>
-				)}
-				{/* In real world case, precise date would be available and would be used with <time> */}
-				<span className={styles.comment_details_date}>
-					<small>{data.createdAt}</small>
-				</span>
-			</header>
-			<div className={styles.comment_content_container}>
-				<p>
-					{typeof commenter === "string" && (
-						<span className={styles.comment_content_commenter}>
-							@{commenter}&nbsp;
+					{isCurrentUser && (
+						<span className={styles.comment_details_label}>
+							<small>you</small>
 						</span>
 					)}
-					{data.content}
-				</p>
-			</div>
-			<div className={styles.comment_score_container}>
-				<button
-					className={`${styles.comment_score_toggle}`}
-					type="button"
-					aria-label="Up vote"
-					aria-pressed={voteInfo.upvotePressed}
-					onClick={handleVote}
-				>
-					<PlusIcon className={styles.icon} />
-				</button>
-				<span className={styles.comment_score_value}>
-					{voteInfo.score}
-				</span>
-				<button
-					className={`${styles.comment_score_toggle}`}
-					type="button"
-					aria-label="Down vote"
-					aria-pressed={voteInfo.downvotePressed}
-					onClick={handleVote}
-				>
-					<MinusIcon className={styles.icon} />
-				</button>
-			</div>
-			{isCurrentUser ? (
-				<div className={styles.comment_useraction_container}>
+					{/* In real world case, precise date would be available and would be used with <time> */}
+					<span className={styles.comment_details_date}>
+						<small>{data.createdAt}</small>
+					</span>
+				</header>
+				<div className={styles.comment_content_container}>
+					<p>
+						{typeof commenter === "string" && (
+							<span className={styles.comment_content_commenter}>
+								@{commenter}&nbsp;
+							</span>
+						)}
+						{data.content}
+					</p>
+				</div>
+				<div className={styles.comment_score_container}>
 					<button
+						className={`${styles.comment_score_toggle}`}
 						type="button"
-						className={`${styles.comment_delete_button} ${styles.comment_action_button}`}
+						aria-label="Up vote"
+						aria-pressed={voteInfo.upvotePressed}
+						onClick={() =>
+							dispatchVoteSelection({ type: "upvote_clicked" })
+						}
 					>
-						<DeleteIcon className={styles.icon} />
-						<span>Delete</span>
+						<PlusIcon className={styles.icon} />
 					</button>
+					<span className={styles.comment_score_value}>
+						{voteInfo.score}
+					</span>
 					<button
+						className={`${styles.comment_score_toggle}`}
 						type="button"
-						className={`${styles.comment_edit_button} ${styles.comment_action_button}`}
+						aria-label="Down vote"
+						aria-pressed={voteInfo.downvotePressed}
+						onClick={() =>
+							dispatchVoteSelection({ type: "downvote_clicked" })
+						}
 					>
-						<EditIcon className={styles.icon} />
-						<span>Edit</span>
+						<MinusIcon className={styles.icon} />
 					</button>
 				</div>
-			) : (
-				<div className={styles.comment_reply_button_wrapper}>
-					<button
-						type="button"
-						className={`${styles.comment_reply_button} ${styles.comment_action_button}`}
-					>
-						<ReplyIcon className={styles.icon} />
-						<span>Reply</span>
-					</button>
-				</div>
-			)}
-		</article>
+				{isCurrentUser ? (
+					<div className={styles.comment_useraction_container}>
+						<button
+							type="button"
+							className={`${styles.comment_delete_button} ${styles.comment_action_button}`}
+						>
+							<DeleteIcon className={styles.icon} />
+							<span>Delete</span>
+						</button>
+						<button
+							type="button"
+							className={`${styles.comment_edit_button} ${styles.comment_action_button}`}
+						>
+							<EditIcon className={styles.icon} />
+							<span>Edit</span>
+						</button>
+					</div>
+				) : (
+					<div className={styles.comment_reply_button_wrapper}>
+						<button
+							type="button"
+							className={`${styles.comment_reply_button} ${styles.comment_action_button}`}
+						>
+							<ReplyIcon className={styles.icon} />
+							<span>Reply</span>
+						</button>
+					</div>
+				)}
+			</article>
+		</>
 	);
 }
---
+++ src/features/CommentsSection/hooks/useVoteInfo.js
@@ -0,0 +1,49 @@
+import { useReducer } from "react";
+
+function voteInfoReducer(state, action) {
+	switch (action.type) {
+		case "upvote_clicked": {
+			return {
+				...state,
+				score:
+					state.downvotePressed === "true"
+						? state.score + 2
+						: state.upvotePressed === "false"
+						? state.score + 1
+						: state.score - 1,
+				upvotePressed:
+					state.upvotePressed === "false" ? "true" : "false",
+				downvotePressed:
+					state.downvotePressed === "true"
+						? "false"
+						: state.downvotePressed,
+			};
+		}
+		case "downvote_clicked": {
+			return {
+				...state,
+				score:
+					state.upvotePressed === "true"
+						? state.score - 2
+						: state.downvotePressed === "false"
+						? state.score - 1
+						: state.score + 1,
+				downvotePressed:
+					state.downvotePressed === "false" ? "true" : "false",
+				upvotePressed:
+					state.upvotePressed === "true"
+						? "false"
+						: state.upvotePressed,
+			};
+		}
+		default: {
+			throw new Error("Unknown action: " + action.type);
+		}
+	}
+}
+
+export default function useVoteInfo(initVal) {
+	const [state, dispatch] = useReducer(voteInfoReducer, initVal);
+
+	return [state, dispatch];
+}
